59730ec89163dab0c4cbe84ec47a80d0a5741627
Moynstain <moynstain2@hotmail.com>
2022-03-04 14:35:58 +0800

    2022/2/22 add

--- 12_Chap6_Practice.cpp
+++ 12_Chap6_Practice.cpp
@@ -2,6 +2,7 @@
 // Created by Moynstain_MBA on 2022/3/4.
 //
 #include "iostream"
+#include "string"
 using namespace std;
 
 
@@ -34,10 +35,59 @@ int practice1(int val){
 
 // practice 2 2022/3/4
 
+bool test_double(string x) {
+    try {
+        double y = stod(x);
+    }
+    catch (const exception&){
+        return false;
+    }
+    return true;
+}
+
+int practice2(int val){
+    cout << "insert 10 donation values in double type: \n";
+    int size = 10;
+    string donation;
+    double dona_array[size];
+    double sum = 0;
+    double avg;
+    int count = 0;
+    // cin >> donation;
+    for (int i = 1; i <= size; i++){
+        cin >> donation;
+        // cout << "test1: " << isnumber(donation) << endl;
+        if (test_double(donation)){
+            //dona_array[i] = donation;
+            sum = sum + stod(donation);
+            avg = sum / i;
+            cout << "value no." << i << "= " << stod(donation) << endl;
+            cout << "sum of previous " << i << " value = " << sum << endl;
+            cout << "average of sum = " << avg << endl;
+            if (stod(donation) > avg){
+                count++;
+                cout << "here's " << count << " value greater than average.\n";
+            }
+        } else{
+            cout << "WARNING: non digit value detected.\n";
+            break;
+        }
+    }
+    cout << endl;
+    cout << "--- average value = " << avg << endl;
+    cout << "--- num_of_value greater than avg = " << count << endl;
+
+    return 0;
+}
+
+// practice 3 2022/3/4
+
 
 
 int main(){
-    practice1(1);
+    // practice1(1); // passed 2022/3/4
+    practice2(1); // passed 2022/3/4
+
 
     return 0;
 }
