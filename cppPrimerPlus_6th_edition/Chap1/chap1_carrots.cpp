a6035aeb10692688db47cf5779380ebd5551867e
Moynstain <moynstain2@hotmail.com>
2022-10-12 18:08:39 +0800

    2022/2/22 add

---
+++ cppPrimerPlus_6th_edition/Chap1/chap1_carrots.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Moynstain_MBA on 2022/10/12.
+//
+
+#include "iostream"
+using namespace std;
+
+int main(){
+    int carrots;
+    carrots = 25;
+    cout << "I have " << carrots << " carrots.\n";
+    carrots-=1;
+    cout << "I now have " << carrots << " carrots.\n";
+    printf("Printf outputs carrot number: %d\n", carrots);
+}
+
+/// compile passed 2022/10/12
---
+++ cppPrimerPlus_6th_edition/Chap1/chap1_myfirst.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Moynstain_MBA on 2022/10/12.
+//
+
+#include "iostream"
+int main(){
+    using namespace std;
+    cout << "this is the first cpp file\n";
+    cout << "it will cover most of the test file in CppPrimerPlus textbook\n";
+    return 0;
+}
+
+/// compile passed 2022/10/12
